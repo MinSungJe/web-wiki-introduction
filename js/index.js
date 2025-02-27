e5d09d7a0ea2dec0a875c3c03ee3f1a7e5b4f046
Minje Sung <minje813@dgu.ac.kr>
2025-02-27 19:38:28 +0900

    feat: 댓글 입력 기능 추가

--- js/index.js
+++ js/index.js
@@ -1,19 +1,36 @@
 console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
+  '%c' +
+    ' __      __  ______   __  __   ______     ' +
+    '\n' +
+    '/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    ' +
+    '\n' +
     "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
+    '\n' +
+    ' \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    ' +
+    '\n' +
+    '  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ ' +
+    '\n' +
+    '   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ ' +
+    '\n' +
     "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
+  'color: #d81b60; font-size: 16px; font-weight: bold;'
 );
 
-console.log("알맞은 스크립트를 작성하세요");
+const commentOneTemplate = document.querySelector('#comment-one');
+const enterButton = document.querySelector('#enter');
+const commentList = document.querySelector('.comment-list');
+
+enterButton.addEventListener('click', () => {
+  const commentClone = commentOneTemplate.content.cloneNode(true);
+  const textValue = document.querySelector('#comment-input').value;
+  if (textValue === '') {
+    alert('댓글을 입력해주세요!');
+    return;
+  }
+
+  commentClone.querySelector('.comment-content').textContent = textValue;
+
+  commentList.appendChild(commentClone);
+  alert('댓글이 등록되었습니다.');
+  document.querySelector('#comment-input').value = '';
+});
